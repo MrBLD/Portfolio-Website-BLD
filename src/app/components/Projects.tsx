a43a8a8e71f4fa6851ace20fec3d597e03e205c7
Bhaskar Lal Das <120587599+MrBLD@users.noreply.github.com>
2024-06-19 23:07:57 +0530

    align

--- src/app/components/Projects.tsx
+++ src/app/components/Projects.tsx
@@ -13,11 +13,11 @@ function Projects() {
       <CardContainer key={`card-${index}`}>
         <CardBody key={`content-${index}`} className='flex flex-row relative rounded-xl w-fit -px-8 border-2 border-purple-900'>
           <CardItem translateZ="60" translateX="-10" className=' rounded-xl -ml-32 flex flex-col z-[11] justify-center -mr-32'>
-            <p className='text-stroke text-sm sm:text-xl font-semibold'>{item.description}</p>
+            <p className='flex flex-row items-center relative rounded-xl w-fit text-stroke text-sm sm:text-xl font-semibold'>{item.description}</p>
           </CardItem>
           {/* <CardItem translateZ="50" translateX='30' className='rounded-xl z-10 flex w-20 h-20 md:w-60 md:h-60 lg:w-96 lg:h-96 mr-8 bg-green-400 overflow-hidden'> */}
           <CardItem translateZ="40" translateX='30' className='relative rounded-xl z-10 flex md:w-60 md:h-60 lg:w-full lg:h-full mr-8 overflow-hidden'>
-            <div className='relative w-full h-full'><Image src={`/images/${item.image}`} alt="Album" width={1920} height={1080}/></div>
+            <div className='pt-auto mt-1 flex flex-row items-center relative w-full h-full'><Image src={`/images/${item.image}`} alt="Album" width={1920} height={1080}/></div>
           </CardItem>
           <CardItem translateZ="60" translateX='-15' className='flex flex-col-reverse text-stroke font-semibold text-lg sm:text-2xl -ml-28 z-10 p-2'>
             <h2>{item.headline}</h2>
